9978d3373783bc74ccd242c2fa6855f464fccbc9
aloisdg <aloisdegouvello@live.fr>
2016-02-21 01:35:22 +0530

    feat(project): Implement MVVM Light

---
+++ Aldeo/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aldeo.ViewModel
+{
+	public class MainViewModel
+	{
+		public string Message { get; set; }
+
+		public MainViewModel()
+		{
+			Message = "He is alive! Alive!";
+		}
+	}
+}
