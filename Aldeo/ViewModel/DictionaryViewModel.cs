ae60679099b66f9edda133e712cd080e5fbaaa5e
aloisdg <aloisdegouvello@live.fr>
2016-09-22 22:54:21 +0200

    feat(dictionary): Update page and vm

--- Aldeo/ViewModel/DictionaryViewModel.cs
+++ Aldeo/ViewModel/DictionaryViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 using Windows.UI.Popups;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
@@ -14,33 +17,55 @@ using SharpDeo;
 namespace Aldeo.ViewModel {
     public class DictionaryViewModel : ViewModelBase {
         public ICommand ClickedSearch { get; set; }
+        public ICommand LoadedCommand { get; set; }
+
         Client client = new Client ();
         public string Answer { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
 
         public DictionaryViewModel() {
             ClickedSearch = new RelayCommand<string> (SearchClickedExecute);
-            Connect();
+            LoadedCommand = new RelayCommand (LoadedExecute);
             Answer = "";
-            Messenger.Default.Register<string>(this, SearchClickedExecute);
+            Title = "";
+            Url = "";
         }
 
-        private async void Connect()
-        {
-            //var task = client.LoginAsync ("beta", "betatest").ConfigureAwait (false);
-            //await task;
+        private async void LoadedExecute() {
+            await ConnectAsync ().ConfigureAwait (false);
+            Messenger.Default.Register<string> (this, SearchClickedExecute);
+            Debug.WriteLine("connected");
+        }
+
+        private Task ConnectAsync() {
+            var task = client.LoginAsync ("toto42", "toto42");
+            return task;
         }
 
         private async void SearchClickedExecute(string input) {
-            //var answer = await SearchAsync (input).ConfigureAwait(false);
+            try {
+                var result = await client.GetDictionaryDefintionAsync (input).ConfigureAwait (false);
 
-            var dialog = new MessageDialog ("answer");
-            await dialog.ShowAsync ();
-            //RaisePropertyChanged (() => Answer);
-        }
+                var dispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
 
-        private async Task<string> SearchAsync(string input) {
-            var result = await client.GetDictionaryDefintionAsync (input).ConfigureAwait (false);
-            return result;
+                await dispatcher.RunAsync (Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                    Title = result.Title;
+                    Answer = result.Extract;
+                    //Url = result.DetailUrl;
+                    //RaisePropertyChanged (() => Title);
+                    //RaisePropertyChanged (() => Answer);
+
+                    RaisePropertyChanged ("Title");
+                    RaisePropertyChanged ("Answer");
+
+                    //RaisePropertyChanged (() => Url);
+                });
+
+            }
+            catch (Exception ex) {
+                Debug.WriteLine(ex.Message);
+            }
         }
     }
 }
